fe719619bcc7efa7a68c4cc1f884114fcba280df
Moelie <m.oelbuettel@gmx.de>
2020-01-10 21:23:15 +0100

    eine Variable erstellt, die den Umsatz vor den Feiertagen berechnet

--- Feiertage.R
+++ Feiertage.R
@@ -16,37 +16,20 @@ library(ggplot2)
 library(dplyr)
 
 #Einlsen der Daten
+
 umsatz <- read_csv("umsatzdaten_gekuerzt.csv", 
-                   col_types = cols(Datum = col_datetime(format = " %Y-%m-%d   "))) #Eingeben des Datums
-
-##Für Warengruppe 1
-w1<-filter(umsatz, Warengruppe==1) #filtern Warengruppe 1
-
-
-
-#scatter plot
-
-ggplot(w1, aes(x=Datum, y=Umsatz)) +
-  geom_point() +
-  geom_smooth(method=lm , color="red", se=FALSE) +
-  geom_label( 
-   data=w1 %>% filter(Umsatz>=200), # alles mit einer Summe ab 2000€ gelabelt
-    aes(label=Datum)
-  )+
-  xlab("Jahr")+
-  ylab("Summe Umsatz (€)")
-ggtitle("Umsatz über den gesamten Zeitraum von w1")# scatterplot
-
-
-#Tabelle nach Umsatz sortieren
-w1<- arrange(w1, desc(Umsatz))
-
+                                 col_types = cols(Datum = col_date(format = "%Y-%m-%d")))
 #überprüfen, ob die Werte durch Feiertage/ Ferien beeinflusst sind
 
-besonderes<-read_delim("Variablen.csv", 
+besonderes<-read_delim("besondere_Tage.csv", 
                                    ";", escape_double = FALSE, col_types = cols(Datum = col_date(format = "%d.%m.%Y")), 
                                    trim_ws = TRUE)
 
 Feiertage<-filter(besonderes, Feiertage==1)
 
-#heraus
+#erstellen einer Variablen mit allen Tagen, die einen Tag vor dem Feiertag sind
+vor_Feiertag<-Feiertage$Datum-days(1)
+vor_Feiertag<-as.data.frame(vor_Feiertag)
+
+#alle Daten vor einem Feiertag
+vor_Feiertag<-umsatz%>% filter(Datum %in% vor_Feiertag$vor_Feiertag)
